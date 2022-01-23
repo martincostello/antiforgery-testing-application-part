6a1a485f2cc44ce8e445d5ddbbef39c26e06f6d0
martincostello <martin@martincostello.com>
2022-01-23 13:51:24 +0000

    Use Minimal Hosting
    Use Minimal Hosting for the sample app.

--- src/TodoApp/Program.cs
+++ src/TodoApp/Program.cs
@@ -1,17 +1,45 @@
 ﻿// Copyright (c) Martin Costello, 2020. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-namespace TodoApp;
+using Microsoft.EntityFrameworkCore;
+using NodaTime;
+using TodoApp.Data;
+using TodoApp.Services;
 
-public static class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IClock>((_) => SystemClock.Instance);
+
+builder.Services.AddScoped<ITodoRepository, TodoRepository>();
+
+builder.Services.AddScoped<ITodoService, TodoService>();
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<TodoContext>((builder) => builder.UseInMemoryDatabase(databaseName: "TodoApp"));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(
-                (webHostBuilder) => webHostBuilder.UseStartup<Startup>());
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapDefaultControllerRoute();
+
+app.Run();
+
+namespace TodoApp
+{
+    public partial class Program
+    {
+        // Expose the Program class for use with WebApplicationFactory<T>
+    }
 }
